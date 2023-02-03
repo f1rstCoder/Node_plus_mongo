ede8fcebcc536faf72e13fa30076c51154272225
Ritwik Pathak <107452129+f1rstCoder@users.noreply.github.com>
2023-02-03 17:51:39 +0530

    Add files via upload
    Hardcoded token authentication

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+const secretKey = "secret123"
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "a sample api"
+    })
+})
+
+app.post("/login", (req, res) => {
+    const user = {
+        id: 1,
+        username: "anil",
+        email: "abc@testing.com"
+    }
+
+    jwt.sign({ user }, secretKey, { expiresIn: '300s' }, (err, token) => {
+        res.json({
+            token
+        })
+    })
+})
+
+app.post("/profile", verifyToken, (req, res) => {
+    jwt.verify(req.token, secretKey, (err, authData) => {
+        if (err) {
+            res.send({ result: "invalid token" })
+        } else {
+            res.json({
+                message: "profile accessed",
+                authData
+            })
+        }
+    })
+})
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authentication'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const token = bearer[1];
+        req.token = token;
+        next();
+    } else {
+        res.send({
+            result: "Token is not valid"
+        })
+    }
+}
+
+app.listen(8002, () => {
+    console.log("app is running on 8002");
+})
